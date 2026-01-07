a031f74b573f8869c9064972d8448f470de4aa14
matias-cederberg <matias.cederberg@outlook.com>
2026-01-07 15:54:26 +0200

    Edit config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://matias-cederberg.github.io/ecg-app',
+    site: 'https://matias-cederberg.github.io',
     base: '/ecg-app'
 });
