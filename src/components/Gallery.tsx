1d57645c49dd56aef0b6bceb02ab1957a994d1e0
matias-cederberg <matias.cederberg@outlook.com>
2026-01-07 16:45:38 +0200

    edit gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -20,6 +20,25 @@ export default function Gallery() {
 
   let sculpture = sculptureList[index];
   return (
-    <div></div>
+    <>
+      <button onClick={handleNextClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img
+        src={sculpture.url}
+        alt={sculpture.alt}
+      />
+    </>
   );
 }
