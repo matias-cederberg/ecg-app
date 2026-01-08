3e2569e6af2ab5a37a531dfd42c6bbf750104d6b
matias-cederberg <matias.cederberg@outlook.com>
2026-01-08 23:24:15 +0200

    Write graph animation

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -9,30 +9,25 @@ export default function Graph({ points }) {
 
   let length;
   let segmentLength;
+  let iterationLength;
 
-  // points = [
-  //   { elevation: 0.0, amplitude: 0.0 },
-  //   { elevation: 0.0, amplitude: 0.02 },
-  //   { elevation: 0.0, amplitude: 0.0 },
-  //   { elevation: 0.0, amplitude: 0.0 },
+  let animProgress = 0; // keep 0 for builds
+  let speed = 0.006;
 
-  //   { elevation: -0.1, amplitude: 0.0 },
-  //   { elevation: 0.3, amplitude: 0.0 },
-  //   { elevation: -0.2, amplitude: 0.0 },
-
-  //   { elevation: 0.0, amplitude: 0.0 },
-  //   { elevation: 0.0, amplitude: 0.06 },
-  //   { elevation: 0.0, amplitude: 0.0 },
-  //   { elevation: 0.0, amplitude: 0.0 },
-  // ]
+  let repeats = 3;
+  let dataLength;
 
   const Sketch = p5i => {
     p5i.setup = () => {
       p5i.createCanvas(512, 512);
 
-      length = p5i.width * 0.75;
+      length = p5i.width * 0.9;
 
-      segmentLength = length / (points.length - 1);
+      dataLength = points.length * repeats;
+
+      iterationLength = length / repeats;
+
+      segmentLength = length / (dataLength - repeats);
 
       xBaseline = (p5i.width - length) * 0.5;
       yBaseline = p5i.height * 0.5;
@@ -41,29 +36,41 @@ export default function Graph({ points }) {
     p5i.draw = () => {
       p5i.background(230, 230, 255);
 
-      p5i.noFill();
-      p5i.strokeWeight(5);
-      p5i.stroke(1);
 
-      for (let i = 0; i < points.length - 1; i++) {
-        drawCurvedLine(p5i, i)
-      };
+
+      for (let iteration = 0; iteration < repeats; iteration++)
+      {
+        p5i.noFill();
+        p5i.strokeWeight(5);
+        p5i.stroke(1);
+
+        for (let i = 0; i < points.length - 1; i++) {
+          drawCurvedLine(p5i, i, iteration)
+        };
+      }
+
+      p5i.noStroke();
+      p5i.fill(230, 230, 255);
+      p5i.rect(0 + p5i.width * animProgress, 0, p5i.width, p5i.height);
+
+      animProgress += speed;
+      if (animProgress > 1) animProgress = 0;
     };
   };
 
-  const drawCurvedLine = (p5i, i) => {
-    let x = xBaseline + segmentLength * i;
+  const drawCurvedLine = (p5i, i, iteration) => {
+    let x = xBaseline + segmentLength * i + iterationLength * iteration;
     let y = yBaseline - p5i.height * points[i].elevation;
 
     let xControl = x;
     let yControl = y;
     if (points[i].amplitude > 0)
       {
-        xControl = xBaseline + ((segmentLength * i) + segmentLength * 0.5);
+        xControl = xBaseline + ((segmentLength * i) + segmentLength * 0.5) + iterationLength * iteration;
         yControl = yBaseline - p5i.height * points[i].amplitude;
       };
 
-    let x2 = xBaseline + segmentLength * (i + 1)
+    let x2 = xBaseline + segmentLength * (i + 1) + iterationLength * iteration
     let y2 = yBaseline - p5i.height * points[i + 1].elevation;
     
     p5i.bezier(x, y, xControl, yControl, xControl, yControl, x2, y2);
