d86583555bdd8e719b9ac6509ff6a5b4ee3da642
matias-cederberg <matias.cederberg@outlook.com>
2026-02-03 19:21:59 +0200

    Deploy

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -9,15 +9,17 @@ export default function Graph({ data }) {
 
   let strokeWeight;
 
-  let startPoint = {x: 0, y: 0};
-  let cursor = {x: 0, y: 0};
+  let startPoint = {};
+  let cursor = {};
+  let graph = {};
 
-  let graphLength;
-  let graphHeight;
+  // scrolling
+  let prevMouseX = 0;
+  let scrollDelta = 0;
 
+  // divider animation
   let animProgress = 0; // keep 0 for builds
   let speed;
-  let speedDebug = 0;
 
   const dividerWidthRatio = 0.1;
   let dividerWidth;
@@ -25,11 +27,12 @@ export default function Graph({ data }) {
   let dividerEnd;
 
   let repeats;
-  let dataLength;
+
 
   const Sketch = p5i => {
     p5i.setup = () => {
 
+      // canvas scaling calculations for different window sizes
       let canvasWidth = Math.min(p5i.windowWidth * 0.95, 1080);
       let canvasHeight = Math.min(p5i.windowWidth * 0.6, 412);
 
@@ -38,27 +41,39 @@ export default function Graph({ data }) {
 
       p5i.createCanvas(canvasWidth, canvasHeight);
 
-      graphLength = canvasHeight / 2;
-      graphHeight = canvasHeight / 2;
+      graph.w = canvasHeight / 1.5;
+      graph.h = canvasHeight / 2;
 
-      dividerStart = -graphLength + (graphLength / 2);
+      let heartRateRatio = 60 / data.heartRate
+      graph.w *= heartRateRatio;
+
+      startPoint = {
+        x: (canvasWidth / 2) - graph.w * 3.35,
+        y: canvasHeight / 1.5
+      };
+
+      // divider animation
+      dividerStart = -graph.w + (graph.w / 2);
       let mult = p5i.windowWidth > 700 ? 5 : 3;
 
-      dividerEnd = graphLength * mult + (graphLength / 2);
+      dividerEnd = graph.w * mult + (graph.w / 2);
 
-      p5i.frameRate(30);
-      speed = 0.001 * graphLength * (data.heartRate / 60);
+      p5i.frameRate(60);
+      speed = 0.001 * graph.w * (data.heartRate / 60);
       animProgress = dividerStart;
 
       dividerWidth = p5i.width * dividerWidthRatio;
 
-      startPoint = {x: (canvasWidth / 2) - graphLength * 3.35, y: canvasHeight / 1.5};
-
+      // initial draw to prevent flicker on resize
       p5i.draw();
     };
 
+
     p5i.draw = () => {
       p5i.background(bgColor);
+
+      startPoint.x += scrollDelta;
+      scrollDelta = 0;
 
       for (let iteration = 0; iteration < repeats; iteration++)
       {
@@ -66,16 +81,13 @@ export default function Graph({ data }) {
         p5i.strokeWeight(strokeWeight);
         p5i.stroke(lineColor);
 
-        cursor.x = startPoint.x + iteration * graphLength;
+        cursor.x = startPoint.x + iteration * graph.w;
         cursor.y = startPoint.y;
 
         for (let i = 0; i < data.points.length; i++) {
           drawCurvedLine(p5i, i, cursor, iteration);
         };
       };
-
-      p5i.noStroke();
-      p5i.fill(bgColor);
 
       drawDivider(p5i);
 
@@ -84,17 +96,31 @@ export default function Graph({ data }) {
       p5i.textSize(18);
       p5i.fill(lineColor);
       
-      //speedDebug += speed * p5i.deltaTime;
       let s = `HR: ${Math.floor(data.heartRate)}`;
       p5i.text(s, p5i.width / 2, p5i.height - 12, 200, 40);
     };
+
+
+    p5i.mouseDragged = () => {
+      scrollDelta = p5i.mouseX - prevMouseX;
+      prevMouseX = p5i.mouseX;
+    }
+
+    p5i.mousePressed = () => {
+      scrollDelta = 0;
+      prevMouseX = p5i.mouseX;
+    }
 
     p5i.windowResized = () => {
       p5i.setup();
     };
   };
 
+
   const drawDivider = (p5i) => {
+    p5i.noStroke();
+    p5i.fill(bgColor);
+
     animProgress += speed * p5i.deltaTime;
     if (animProgress > dividerEnd) animProgress = dividerStart;
 
@@ -105,21 +131,24 @@ export default function Graph({ data }) {
     p5i.rect(animProgress, 0, dividerWidth, p5i.height);
   };
 
-  const drawCurvedLine = (p5i, i, cursor, iteration) => {
-    let x = cursor.x;
-    
-    let y = cursor.y;
 
-    let x2 = x + data.points[i].duration * graphLength;
-    let y2 = startPoint.y - data.points[i].elevation * graphHeight;
+  const drawCurvedLine = (p5i, i, cursor, iteration) => {
+
+    let {x, y} = cursor;
+    let {amplitude, duration, elevation} = data.points[i]
+
+    let x2 = x + duration * graph.w;
+    let y2 = startPoint.y - elevation * graph.h;
 
     let controlPoint = {x: x, y: y}
-    if (data.points[i].amplitude > 0)
+
+    if (amplitude > 0)
     {
+      // shift endpoints to make line look smoother
       x, y -= .3;
       x2, y2 -= .3;
-      controlPoint.x = x + (data.points[i].duration * graphLength) / 2;
-      controlPoint.y = startPoint.y - data.points[i].amplitude * graphHeight;
+      controlPoint.x = x + (duration * graph.w) / 2;
+      controlPoint.y = startPoint.y - amplitude * graph.h;
     };
 
     cursor.x = x2;
@@ -128,9 +157,10 @@ export default function Graph({ data }) {
     p5i.bezier(x, y, controlPoint.x, controlPoint.y, controlPoint.x, controlPoint.y, x2, y2);
 
     if (i == data.points.length - 1) {
-      p5i.line(x2, y2, startPoint.x + graphLength * (iteration + 1), y2);
+      p5i.line(x2, y2, startPoint.x + graph.w * (iteration + 1), y2);
     }
   };
+
 
   useEffect(() => {
     const newp5 = new p5(Sketch, processingRef.current);
--- src/components/data.js
+++ src/components/data.js
@@ -21,24 +21,24 @@ export const ecgList = [
   ]
 },
 {
-  name: 'Sinusrytmi',
+  name: 'Sinusbradykardia',
   description: '',
-  heartRate: 76,
+  heartRate: 42,
   points: [
     // P
-    { elevation: 0.00, amplitude: 0.07, duration: 0.13 },
+    { elevation: 0.00, amplitude: 0.10, duration: 0.08 },
     // P-Q
-    { elevation: 0.00, amplitude: 0.00, duration: 0.05 },
+    { elevation: 0.00, amplitude: 0.00, duration: 0.04 },
     // Q
-    { elevation: -0.10, amplitude: 0.00, duration: 0.04 },
+    { elevation: -0.10, amplitude: 0.00, duration: 0.02 },
     // R
-    { elevation: 0.90, amplitude: 0.00, duration: 0.03 },
+    { elevation: 0.90, amplitude: 0.00, duration: 0.015 },
     // S
-    { elevation: 0.00, amplitude: 0.00, duration: 0.03 },
+    { elevation: 0.00, amplitude: 0.00, duration: 0.015 },
     // S-T
-    { elevation: 0.00, amplitude: 0.00, duration: 0.15 },
+    { elevation: 0.00, amplitude: 0.00, duration: 0.08 },
     // T
-    { elevation: 0.00, amplitude: 0.16, duration: 0.24 },
+    { elevation: 0.00, amplitude: 0.16, duration: 0.12 },
   ]
 },
 {
